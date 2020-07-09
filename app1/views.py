b5a8e0b27a7e7f70e9a17f68e5bd464c01d93b02
shaju george <shajugeorge45@gmail.com>
2020-07-09 22:17:34 +0530

    SQLlite added post and csrf

--- app1/views.py
+++ app1/views.py
@@ -6,8 +6,8 @@ def home(request ) :
     return render (request,'home.html',{'name':'shaju'})
 
 def add(request) :
-    val1 =int(request.GET["number1"])
-    val2 = int(request.GET["number2"])
+    val1 =int(request.POST["number1"])
+    val2 = int(request.POST["number2"])
     result = val1 +val2
 
     return render (request,'result.html',{'result':result })
