4fbd7ef4b60118bc011ffd5b88cf475b5fcfa8b5
shaju george <shajugeorge45@gmail.com>
2020-07-11 16:12:52 +0530

    created postgre data base  v2

--- postgre1/settings.py
+++ postgre1/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tour',
+    'app1',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'postgre1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD' : '6659',
+        'HOST' : 'localhost',
+
     }
 }
 
